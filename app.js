3cfa65971dd418e49e1668b907ecebd1da647841
Kyle L. Jensen <kljensen@gmail.com>
2013-05-23 08:23:49 -0400

    Add some comments to the code

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
 
-var clientId = 0;
-var clients = {};
-
 var template = ' \
 <!DOCTYPE html> <html> <body> \
 	<script type="text/javascript"> \
@@ -14,26 +11,30 @@ var template = ' \
 </script> </body> </html>';
 
 app.get('/', function(req, res) {
-	res.send(template);
+	res.send(template);  // <- Return the static template above
 });
 
+var clientId = 0;
+var clients = {};  // <- Keep a map of attached clients
+
+// Called once for each new client. Note, this response is left open!
 app.get('/events/', function(req, res) {
 	req.socket.setTimeout(Infinity);
     res.writeHead(200, {
-    	'Content-Type': 'text/event-stream',
+    	'Content-Type': 'text/event-stream',  // <- Important headers
     	'Cache-Control': 'no-cache',
     	'Connection': 'keep-alive'
     });
     res.write('\n');
-    clients[++clientId] = res;
-    req.on("close", function(){delete clients[clientId]});
+    clients[++clientId] = res;  // <- Add this client to those we consider "attached"
+    req.on("close", function(){delete clients[clientId]});  // <- Remove this client when he disconnects
 });
 
 setInterval(function(){
 	var msg = Math.random();
 	console.log("Clients: " + Object.keys(clients) + " <- " + msg);
 	for (clientId in clients) {
-		clients[clientId].write("data: "+ msg + "\n\n");
+		clients[clientId].write("data: "+ msg + "\n\n"); // <- Push a message to a single attached client
 	};
 }, 2000);
 
