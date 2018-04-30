6c52b8c1efd2dda06e2ac9b2e1949329582acf62
Kyle L. Jensen <kljensen@gmail.com>
2018-04-30 15:12:11 +0200

    Use Number.MAX_VALUE instead of Infinity for timeout. Closes #4

--- app.js
+++ app.js
@@ -10,33 +10,35 @@ var template = ' \
 		    }; \
 </script> </body> </html>';
 
-app.get('/', function(req, res) {
-	res.send(template);  // <- Return the static template above
+app.get('/', function (req, res) {
+	res.send(template); // <- Return the static template above
 });
 
 var clientId = 0;
-var clients = {};  // <- Keep a map of attached clients
+var clients = {}; // <- Keep a map of attached clients
 
 // Called once for each new client. Note, this response is left open!
-app.get('/events/', function(req, res) {
-	req.socket.setTimeout(Infinity);
-    res.writeHead(200, {
-    	'Content-Type': 'text/event-stream',  // <- Important headers
-    	'Cache-Control': 'no-cache',
-    	'Connection': 'keep-alive'
-    });
-    res.write('\n');
-    (function(clientId) {
-        clients[clientId] = res;  // <- Add this client to those we consider "attached"
-        req.on("close", function(){delete clients[clientId]});  // <- Remove this client when he disconnects
-    })(++clientId)
+app.get('/events/', function (req, res) {
+	req.socket.setTimeout(Number.MAX_VALUE);
+	res.writeHead(200, {
+		'Content-Type': 'text/event-stream', // <- Important headers
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive'
+	});
+	res.write('\n');
+	(function (clientId) {
+		clients[clientId] = res; // <- Add this client to those we consider "attached"
+		req.on("close", function () {
+			delete clients[clientId]
+		}); // <- Remove this client when he disconnects
+	})(++clientId)
 });
 
-setInterval(function(){
+setInterval(function () {
 	var msg = Math.random();
 	console.log("Clients: " + Object.keys(clients) + " <- " + msg);
 	for (clientId in clients) {
-		clients[clientId].write("data: "+ msg + "\n\n"); // <- Push a message to a single attached client
+		clients[clientId].write("data: " + msg + "\n\n"); // <- Push a message to a single attached client
 	};
 }, 2000);
 
